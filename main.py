bf1b60fdd31173d4b084732e1ee46879bbf0054a
LuzPaty02 <luzpatyhr@gmail.com>
2023-09-24 16:28:06 -0600

    firstAdaptationsReaderfile

--- main.py
+++ main.py
@@ -1,36 +1,85 @@
-import flet as ft
-import os
 
-for file in range(476):
-    os.rename(f"Definite_German_Articles/{file}", f"Definite_German_Articles/{file}.jpg")
+import csv
+import sqlite3
 
-""" 
-import flet as ft
 
-def main(page: ft.Page):
-    
-    text: ft.Container = ft.Container(
-        border = ft.border.all(1, "#FFFF00"),
-        expand = True,
-        content = ft.Text(
-            "Hola Paty",
-            font_family = "Verdana",
-            font_size = 35,
-            font_color = "#FFFF00"
+class Database:
+    def __init__(self) -> None:
+        self._csv_path: str = "ArtikkelApp/InglesAleman.csv"
+        self._db_path: str = "ArtikkelApp/InglesAleman.db"
+        self.search_results: dict[str, list[str]] = {
+            "EnglishWord": [],
+            "GermanWord": []
+        }
+
+
+    def _csv_to_database(self) -> None:
+        """
+        Convierte un archivo .csv en una base de datos .db con SQLite3.
+        """
+
+        # Se crea la conexión y el cursor
+        _connection: sqlite3.Connection = sqlite3.connect(self._db_path)
+        _cursor: sqlite3.Cursor = _connection.cursor()
+
+        # Se crea una lista temporal para almacenar los datos
+        _temp_list: list[tuple[str, str]] = []
+
+        with open(self._csv_path, newline="", encoding="utf-8") as csvfile:
+            _reader = csv.reader(csvfile, delimiter="\t", quotechar='"')
+            for row in _reader:
+                english_words_Col = row[0].strip()
+                german_words_Col = row[1].strip()
+
+            
+                _temp_list.append((english_words_Col, german_words_Col))
+
+
+        # Se crea la tabla y se insertan los datos
+        _cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS vocabulary (EnglishWord TEXT, GermanWord TEXT) '''
         )
-    )
 
-    page.add(
-        ft.Column(
-            controls = [
-                text
-            ]
+        _connection.commit()
+
+        # Se elimina la lista temporal para liberar memoria
+
+        # Se insertan los datos en la tabla
+        _cursor.executemany(
+            ''' INSERT INTO vocabulary VALUES (?, ?) ''', _temp_list
         )
-    )
 
-    page.update()
+        _connection.commit()
+
+        del _temp_list
+
+        # Prueba de que la base de datos se creó correctamente
+        # Por defecto, se comenta para evitar que se imprima en la terminal
+        _test_printing = _cursor.execute(
+            ''' SELECT * FROM vocabulary '''
+         )
+
+        for row in _test_printing:
+             print(row)
+
+        _cursor.close()
+        _connection.close()
 
 
-if __name__ == "__main__":
-    ft.app(target = main, view = ft.AppView.WEB_BROWSER)
-     """
+    def search_vocab(self, kanji: str) -> dict[list[str], list[str]]:
+
+        _connection: sqlite3.Connection = sqlite3.connect(self._db_path)
+        _cursor: sqlite3.Cursor = _connection.cursor()
+
+        list_search_results: list[tuple[str, str]] = _cursor.execute(
+            '''SELECT * FROM vocabulary WHERE kanji LIKE ?''', ('%' + kanji + '%',)
+        ).fetchall()
+
+        for result in list_search_results:
+            self.search_results['kanji'].append(result[0])
+            self.search_results['meaning'].append(result[1])
+
+        _cursor.close()
+        _connection.close()
+
+        return self.search_results 
